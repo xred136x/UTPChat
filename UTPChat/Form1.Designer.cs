395263d509175583394b2e72948713a20fbb2d02
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2024-01-24 21:13:44 +0500

    Добавьте файлы проекта.

---
+++ UTPChat/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace UTPChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.txtAllMes = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.mtIPAddress = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(130, 12);
+            this.numPort.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(83, 20);
+            this.numPort.TabIndex = 1;
+            this.numPort.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            // 
+            // txtAllMes
+            // 
+            this.txtAllMes.Location = new System.Drawing.Point(13, 39);
+            this.txtAllMes.Multiline = true;
+            this.txtAllMes.Name = "txtAllMes";
+            this.txtAllMes.Size = new System.Drawing.Size(775, 325);
+            this.txtAllMes.TabIndex = 2;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(13, 390);
+            this.txtMes.Multiline = true;
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(623, 48);
+            this.txtMes.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSend.Location = new System.Drawing.Point(659, 390);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(129, 48);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(237, 9);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(128, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // mtIPAddress
+            // 
+            this.mtIPAddress.Location = new System.Drawing.Point(13, 11);
+            this.mtIPAddress.Name = "mtIPAddress";
+            this.mtIPAddress.Size = new System.Drawing.Size(111, 20);
+            this.mtIPAddress.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mtIPAddress);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtAllMes);
+            this.Controls.Add(this.numPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.TextBox txtAllMes;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox mtIPAddress;
+    }
+}
+
---
+++ UTPChat/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UTPChat
+{
+    public partial class Form1 : Form
+    {
+        //IPAddress remoteIP;
+        IPEndPoint remoteEndPoint;
+        public Form1()
+        {
+            InitializeComponent();
+            //btnConnect_Click(null, null);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            UdpClient udpClient = new UdpClient();
+            byte[] buffer = Encoding.Unicode.GetBytes(txtMes.Text);
+            udpClient.Send(buffer, buffer.Length, remoteEndPoint);
+            udpClient.Close();
+            //txtAllMes.Lines.Append("--"+txtMes.Text);
+            txtMes.Text = "";
+        }
+        Thread threadRec;
+        CancellationTokenSource cts;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            remoteEndPoint = new IPEndPoint(IPAddress.Parse(mtIPAddress.Text),(int)numPort.Value);
+            /*threadRec = new Thread(new ThreadStart(ThreadReceive));
+            threadRec.IsBackground = true;*/
+            cts = new CancellationTokenSource();
+            Task.Run(() => ThreadReceive(cts.Token));
+            //threadRec.Start();
+            
+        }
+        void ThreadReceive(CancellationToken token)
+        {
+            try
+            {
+                while (true)
+                {
+                    UdpClient udpClient = new UdpClient((int)numPort.Value);
+                    IPEndPoint remEndPoint = new IPEndPoint(IPAddress.Any, (int)numPort.Value);
+                    byte[] response = udpClient.Receive(ref remEndPoint); //
+                    Invoke((MethodInvoker)delegate
+                    {
+                        //txtAllMes.Lines.Append(Encoding.Unicode.GetString(response));
+                        txtAllMes.Text += Encoding.Unicode.GetString(response)+"\r\n";
+                    });
+                    udpClient.Close();
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //threadRec.Abort();
+            cts.Cancel();
+        }
+    }
+}
